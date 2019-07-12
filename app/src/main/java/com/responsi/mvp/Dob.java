8b3cc1e33da9d9a84bda510aa91870931ced8d73
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2019-07-12 08:31:51 +0700

    Responsi mvp

---
+++ app/src/main/java/com/responsi/mvp/Dob.java
@@ -0,0 +1,29 @@
+package com.responsi.mvp;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class Dob{
+
+	@SerializedName("date")
+	private String date;
+
+	@SerializedName("age")
+	private int age;
+
+	public void setDate(String date){
+		this.date = date;
+	}
+
+	public String getDate(){
+		return date;
+	}
+
+	public void setAge(int age){
+		this.age = age;
+	}
+
+	public int getAge(){
+		return age;
+	}
+}
---
+++ app/src/main/java/com/responsi/mvp/People.java
@@ -0,0 +1,30 @@
+package com.responsi.mvp;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+
+public class People{
+
+	@SerializedName("results")
+	private List<ResultsItem> results;
+
+	@SerializedName("info")
+	private Info info;
+
+	public void setResults(List<ResultsItem> results){
+		this.results = results;
+	}
+
+	public List<ResultsItem> getResults(){
+		return results;
+	}
+
+	public void setInfo(Info info){
+		this.info = info;
+	}
+
+	public Info getInfo(){
+		return info;
+	}
+}
